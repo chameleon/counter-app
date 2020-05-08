9634d02bec45f1b648872fced6c309523ef350a7
joe <carrborojoe@gmail.com>
2020-05-08 16:33:17 -0700

    second initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+//added after running [ install i bootstrap@4.1.1 ]
+import "bootstrap/dist/css/bootstrap.css";
+
+//import our custom component
+import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
