0a7daab3fa718f6c83742959e04dd2197b4cb358
joe <carrborojoe@gmail.com>
2020-05-09 00:04:57 -0700

    commit prior to Controlled Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@
 // Class below will inherit methods from 'React'
 //  like the React.createElemnt is used below behind the scenes
 import React, {Component} from "react";
+// import Counters from "./counters";
 
 //shortcut  cc
 //class has state(data) and render(UI elements)
@@ -9,30 +10,44 @@ class Counter extends Component {
   //add the stat property - stat is a special property in react that holds any data this component needs
   // state property set to = data object
   state = {
-    value: this.props.value,
+    /value: this.props.counter.value,
   };
 
-  handleIncrement = (productID) => {
+  handleIncrement = productID => {
     this.setState({value: this.state.value + 1});
-    console.log(productID);
+    // console.log(productID);
   };
 
   render() {
-    console.log("props", this.props);
+    console.log("props", this.props.counter);
     //add css classes dynamically - depending on count value
     //Had synamic classes here bloating the render() so refactored below.
-    // try to keep render clean and not bloated with respoinsibilities.
+    // try to keep render clean and not bloated with respoinsibilities
+
     return (
       //'this' refs current object
       //use js array method MAP to loop through tags array and map it to an LI
-      <div>
+      <div>  
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         {/* Reference the function - don't actually call it with"()" like in html   */}
         <button
-          onClick={() => this.handleIncrement({id: 1})}
+          onClick={() => this.handleIncrement({id: this.props.counter.id})}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          //onClick is being raised to a method in parent
+          // parent Components.jsx passed a reference to it's delete method via prop named onDelete
+          //THis worked but had no ID
+          //onClick={this.props.onDelete}
+          //This won't work - need arrow func
+          //onClick={this.props.onDelete(this.props.id)}
+          // So...
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     ); // JSX expression - not string
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,41 @@ class Counters extends Component {
     ],
   };
 
+  //   We should always let the CounterS component edit it's own state.
+  //  so we 'raised' the event handler for counter.jsx
+  handleDelete = counterId => {
+    console.log("deletedd", counterId);
+    // this.setState =  a whole new version of the counters array
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters});
+  };
+
   render() {
     return (
       <React.Fragment>
-        {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected />
+        <button
+          onClick={this.handlReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            counter={counter} //pass the whole object and you'll nver have to update this
+            //id={counter.id}
+            //value={counter.value}
+            //selected //same as selected="true"
+            onDelete={this.handleDelete}
+          />
         ))}
       </React.Fragment>
     );
